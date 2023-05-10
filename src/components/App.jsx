db95c40dfad8a63754ad1fa2ebcc1f5a58caa046
Elena Us <Elena88555@gmail.com>
2023-05-11 00:05:54 +0300

    statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import  {FeedbackOptions} from './FeedbackOptions/FeedbackOptions';
-import {Statistics} from './Statistics/Statistics';
-import {Section} from "./Section/Section";
-import {Notification} from "./Notification/Notification";
-import css from "./App.module.css";
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import css from './App.module.css';
 
- class App extends Component {
+class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-
-
   handleClickBtn = event => {
-   const nameBtm = event.currentTarget.name;
-console.log(event.target)
-    if ( nameBtm) {
-      this.setState(prevState => ({ [nameBtm]: prevState[ nameBtm] + 1 }));
+    const nameBtm = event.currentTarget.name;
+    console.log(event.target);
+    if (nameBtm) {
+      this.setState(prevState => ({ [nameBtm]: prevState[nameBtm] + 1 }));
     }
   };
 
@@ -26,7 +24,6 @@ console.log(event.target)
     const total = Object.values(this.state).reduce((acc, el) => acc + el, 0);
     return total;
   };
-
 
   countPositiveFeedbackPercentage = () => {
     const totalCounter = this.countTotalFeedback();
@@ -36,7 +33,7 @@ console.log(event.target)
     if (totalCounter > 0) {
       persentpage = Math.round((goodFeedback / totalCounter) * 100);
     }
-    return `${persentpage}%`
+    return `${persentpage}%`;
   };
 
   render() {
@@ -44,34 +41,32 @@ console.log(event.target)
     const { good, neutral, bad } = this.state;
     const countTotalFeedback = this.countTotalFeedback();
     const handleClickBtn = this.handleClickBtn;
-    const countPositiveFeedbackPercentage = this.countPositiveFeedbackPercentage()
+    const countPositiveFeedbackPercentage =
+      this.countPositiveFeedbackPercentage();
     return (
-      <div className={css.mainContainer} >
+      <div className={css.mainContainer}>
         <div className={css.container}>
-        <Section title="Please leave feedback">
-<FeedbackOptions
-          options={options}
-          onLeaveFeedback={handleClickBtn}
-        ></FeedbackOptions>
-      </Section>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={options}
+              onLeaveFeedback={handleClickBtn}
+            ></FeedbackOptions>
+          </Section>
 
-<Section title="Statistics">
-{countTotalFeedback > 0 ? (<Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={countTotalFeedback}
-          positivePercentage={countPositiveFeedbackPercentage}
-        ></Statistics>) : (
-
-        <Notification message="There is no feedback" /> )
-
-}
-
-</Section>
+          <Section title="Statistics">
+            {countTotalFeedback > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback}
+                positivePercentage={countPositiveFeedbackPercentage}
+              ></Statistics>
+            ) : (
+              <Notification message="There is no feedback" />
+            )}
+          </Section>
         </div>
-
-       
       </div>
     );
   }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,44 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from "./Statistics.module.css"
+import css from './Statistics.module.css';
+import { ItemStat } from 'components/ItemStat/ItemStat';
 
-export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
-      <ul className={css.itemList}>
-      <li className={css.list}>
-        <p className={css.nameOption}>
-         Good:
-          <span className={css.value}>{good}</span>
-        </p>
-      </li>
-      <li className={css.list}>
-        <p className={css.nameOption}>
-        Neutral:
-          <span className={css.value}>{neutral}</span>
-        </p>
-      </li>
-      <li className={css.list}>
-        <p className={css.nameOption}>
-        Bad:
-          <span className={css.value}>{bad}</span>
-        </p>
-      </li>
-      <li className={css.list}>
-        <p className={css.nameOption}>
-        Total:
-          <span className={css.value}>{total}</span>
-        </p>
-      </li>
-      <li className={css.list}>
-        <p className={css.nameOption}>
-        PositivePercentage:
-          <span className={css.value}>{positivePercentage}</span>
-        </p>
-      </li>
+    <ul className={css.itemList}>
+      <ItemStat ><p >Good:</p><span className = {css.value}>{good}</span></ItemStat>
+      <ItemStat ><p >Neutral:</p><span className = {css.value}>{neutral}</span></ItemStat>
+      <ItemStat ><p >Bad:</p><span className = {css.value}>{bad}</span></ItemStat>
+      <ItemStat ><p >Total:</p><span className = {css.value}>{total}</span></ItemStat>
+      <ItemStat ><p >Positive feedback:</p><span className = {css.value}>{positivePercentage}</span></ItemStat>
+   
+   
+   
+      {/* <li className = {css.nameOption}>Good: <span className = {css.value}>{good}</span> </li>
+      <li className = {css.nameOption}>Neutral: <span className = {css.value}>{neutral}</span> </li>
+      <li className = {css.nameOption}>Bad: <span className = {css.value}>{bad}</span> </li>
+      <li className = {css.nameOption}>Total: <span className = {css.value}>{total}</span> </li>
+      <li className = {css.nameOption}>Positive feedback: <span className = {css.value}>{positivePercentage}</span> </li> */}
     </ul>
   );
 };
-
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
@@ -47,7 +36,3 @@ Statistics.propTypes = {
   total: PropTypes.number.isRequired,
   positivePercentage: PropTypes.string.isRequired,
 };
-
-
-
-
